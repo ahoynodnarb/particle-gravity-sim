7f3377f438b5d436e197f545b451cd87c8691df0
Brandon Yao <ahoynodnarb@gmail.com>
2026-01-28 20:42:21 -0600

    No longer storing old accelerations since they should be zeroed out after the verlet step

--- physics_solver.cpp
+++ physics_solver.cpp
@@ -27,13 +27,10 @@ const std::vector<particle> &physics_solver::get_particles() const {
 }
 // TODO: Maybe an epsilon to check against the distance?
 void physics_solver::step() {
-  std::vector<vec2> initial_accelerations;
-  initial_accelerations.reserve(particles_.size());
-  for (const auto &p : particles_) {
-    initial_accelerations.push_back(p.a);
-  }
   for (unsigned i = 0; i < particles_.size(); ++i) {
     particle &p = particles_[i];
+    vec2 x_step = p.v * time_delta + 0.5 * p.a * (time_delta * time_delta);
+    vec2 v_step = 0.5 * p.a * time_delta;
     // since apply_gravity applies to both particles, each particle p has
     // already accumulated all the gravitational forces for previous particles,
     // and the inner loop accumulates the gravitational forces for the next
@@ -49,12 +46,10 @@ void physics_solver::step() {
     // since no more particles rely on any properties of particle p, we are
     // completely safe to do the verlet step
     if (!p.fixed) {
-      vec2 &old_a = initial_accelerations[i];
-      vec2 x_step = p.v * time_delta + 0.5 * old_a * (time_delta * time_delta);
-      vec2 v_step = 0.5 * (old_a + p.a) * time_delta;
+      v_step += 0.5 * p.a * time_delta;
       p.x += x_step;
       p.v += v_step;
-      p.a = old_a;
+      p.a = vec2(0, 0);
     }
   }
 }
